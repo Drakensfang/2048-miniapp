cd4cc4b15b6aa0939c2c5bc7cec8ef278d8be928
Seofon <tohtzx@gmail.com>
2025-11-28 16:15:39 +0800

    deleted route

--- app/.well-known/farcaster.json/route.ts
+++
@@ -1,7 +0,0 @@
-{
-  "accountAssociation": {
-    "header": "eyJmaWQiOjI0NjQ3MSwidHlwZSI6ImN1c3RvZHkiLCJrZXkiOiIweDQzQTgwOTQ1OTE0M2RBYTJFMDM5Njk1NTAwYzJBNjNBZTMyZTMxRDEifQ",
-    "payload": "eyJkb21haW4iOiIyMDQ4LW1pbmlhcHAtbGl2aWQudmVyY2VsLmFwcCJ9",
-    "signature": "Aj5Ct2B+ed7pvLXB8KN0YwmnTF/RSRwQjssEug/7hQYbrF9HxaX4UDsHFB1AR1+cIWNzkyBcP49SA6SI/0JnaRw="
-  }
-}
