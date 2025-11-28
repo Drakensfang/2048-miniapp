d4e53801ceb674606ce52369c03e26d19d16df2b
Seofon <tohtzx@gmail.com>
2025-11-28 15:29:25 +0800

    Made changes to farcaster.json

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -1,15 +1,6 @@
-import { NextResponse } from "next/server";
+import { withValidManifest } from "@coinbase/onchainkit/minikit";
 import { minikitConfig } from "../../../minikit.config";
 
 export async function GET() {
-  const response = NextResponse.json({
-    accountAssociation: minikitConfig.accountAssociation,
-    miniapp: minikitConfig.miniapp,
-  });
-  
-  response.headers.set("Content-Type", "application/json");
-  response.headers.set("Access-Control-Allow-Origin", "*");
-  response.headers.set("Cache-Control", "public, max-age=3600");
-  
-  return response;
+  return Response.json(withValidManifest(minikitConfig));
 }
